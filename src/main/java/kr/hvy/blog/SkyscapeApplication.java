c1478cae20a12cc3d06885d4765ea49d0c67c056
Namwon Kim <knw1234@gmail.com>
2022-04-11 10:35:56 +0900

    init

---
+++ src/main/java/kr/hvy/blog/SkyscapeApplication.java
@@ -0,0 +1,13 @@
+package kr.hvy.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyscapeApplication.class, args);
+	}
+
+}
