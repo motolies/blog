74df0ba13ba3cfada1e919f0ed402930fdb5367c
Namwon Kim <knw1234@gmail.com>
2022-04-14 17:54:09 +0900

    소스 정리

--- src/main/java/kr/hvy/blog/entity/provider/CustomUUIDProvider.java
+++ src/main/java/kr/hvy/blog/entity/provider/CustomUUIDProvider.java
@@ -3,15 +3,11 @@ package kr.hvy.blog.entity.provider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.HibernateException;
-import org.hibernate.MappingException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 import org.hibernate.query.spi.NativeQueryImplementor;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.type.Type;
 
 import java.io.Serializable;
-import java.util.Properties;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,10 +18,5 @@ public class CustomUUIDProvider implements IdentifierGenerator {
         NativeQueryImplementor rows = session.createSQLQuery("SELECT FN_ORDERED_UUID()");
         return (byte[]) rows.list().get(0);
     }
-
-    @Override
-    public void configure(Type type, Properties params, ServiceRegistry serviceRegistry) throws MappingException {
-    }
-
 
 }
