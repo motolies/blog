6528f3f9e5f8ad0c66b0d624c7bc9504fbdf16d2
motolies <knw1234@gmail.com>
2022-05-01 02:07:11 +0900

    Category model 정리

--- src/main/java/kr/hvy/blog/entity/Category.java
+++ src/main/java/kr/hvy/blog/entity/Category.java
@@ -1,9 +1,6 @@
 package kr.hvy.blog.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.*;
 import kr.hvy.blog.annotation.SpecialCharacterListener;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +14,7 @@ import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 @Entity
@@ -30,11 +28,8 @@ public class Category implements Serializable {
 
     @Column(name = "Id", nullable = false, length = 32)
     @Id
-    //	@GeneratedValue(generator = "CATEGORY_ID_GENERATOR")
-    //	@org.hibernate.annotations.GenericGenerator(name = "CATEGORY_ID_GENERATOR", strategy = "native")
     private String id;
 
-    @JsonProperty("text")
     @Column(name = "Name", nullable = false, length = 64)
     private String name;
 
@@ -55,9 +50,6 @@ public class Category implements Serializable {
     @GeneratedValue(generator = "CATEGORY_PID_CATEGORYID_GENERATOR")
     @GenericGenerator(name = "CATEGORY_PID_CATEGORYID_GENERATOR", strategy = "foreign", parameters = @Parameter(name = "property", value = "p"))
     private String pId;
-
-    //	@Column(name = "pId", nullable = true, length = 32)
-    //	private String pId;
 
     @JsonProperty("pId")
     public String getPId() {
@@ -94,55 +86,22 @@ public class Category implements Serializable {
     @org.hibernate.annotations.LazyCollection(org.hibernate.annotations.LazyCollectionOption.TRUE)
     private java.util.List<Category> category = new java.util.ArrayList<Category>();
 
-    public static String LeftPadding(String name, int depth) {
-
-        if (depth < 0)
-            return name;
-
-        String pad = "";
-        for (int i = 0; i < depth; i++) {
-            pad += "ㅤ";
-        }
-
-        if (pad.length() > 0)
-            return MessageFormat.format("{0}┗ㅤ{1}", pad, name);
-        else
-            return name;
-
-    }
 
     @Formula("(select count(*) from Content as con where con.categoryId = id )")
     private int contentCount;
 
-    @Transient
-    private Map<String, Object> data = new HashMap<String, Object>();
-
-    @JsonProperty("data")
-    public Map<String, Object> getData() {
-        return this.data;
-    }
-
-    @Transient
-    private Map<String, Object> recv = new HashMap<String, Object>();
-
-    @JsonProperty("data")
-    public void setData(Map<String, Object> data) {
-        // https://offbyone.tistory.com/370
-
-        for (Map.Entry<String, Object> entry : data.entrySet()) {
-            this.recv.put(entry.getKey(), entry.getValue());
+    @JsonGetter
+    private String getTreeName() {
+        long level = this.fullName.chars().filter(f -> f == '/').count() - 3;
+        String prefixLevel = "";
+        for (int i = 0; i < level; i++) {
+            // 공백 특수문자
+            Character c = (char) Integer.parseInt("3000", 16);
+            prefixLevel += c;
         }
-
-        Object oOrder = data.get("order");
-        if (oOrder != null)
-            this.order = Integer.parseInt(oOrder.toString());
+        return prefixLevel + "└─" + this.name;
     }
 
-    @PostLoad
-    private void onLoad() {
-        data.put("addHTML", MessageFormat.format("<span class=\"badge badge-primary\">{0}</span>", Integer.toString(contentCount)));
-        data.put("order", order);
-    }
 
     @PrePersist
     protected void onCreate() {
@@ -156,9 +115,6 @@ public class Category implements Serializable {
 
     public void cleanUp() {
         this.fullName = "/" + this.name + "/";
-
-        //		this.order = Integer.parseInt(this.recv.get("order").toString());
-
     }
 
 }
