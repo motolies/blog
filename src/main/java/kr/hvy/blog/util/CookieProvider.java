d3d59d71fef014aea75222f4579762e54ebc82a7
Namwon Kim <knw1234@gmail.com>
2022-05-27 16:46:18 +0900

    cookie 쓸 때 도메인 체크해서 각각 써주기(테스트)

--- src/main/java/kr/hvy/blog/util/CookieProvider.java
+++ src/main/java/kr/hvy/blog/util/CookieProvider.java
@@ -1,17 +1,18 @@
 package kr.hvy.blog.util;
 
+import com.google.common.net.InternetDomainName;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
 
 @Slf4j
 @Component
 public class CookieProvider {
     //https://velog.io/@kwakwoohyun/%EC%9D%B4%EC%8A%88%EC%B2%98%EB%A6%AC-spring-boot-local-cookie
-    private static final String DOMAIN = "hvy.kr";
     private static final String COOKIE_PATH = "/";
     private static final boolean READONLY = true;
     private static int MAX_AGE;
@@ -21,12 +22,23 @@ public class CookieProvider {
         this.MAX_AGE = Integer.parseInt(age);
     }
 
+    private static String getRootDomain(String refererUrl) {
+        try {
+            URI uri = new URI(refererUrl);
+            return InternetDomainName.from(uri.getHost()).topDomainUnderRegistrySuffix().toString();
+        } catch (Exception e) {
+            log.error("getRootDomain error : {}", e.getMessage());
+            return null;
+        }
+    }
+
     public static ResponseCookie setSpringCookie(HttpServletRequest request, String name, String value) {
-        if (request.getHeader("referer").contains(DOMAIN)) {
+        String domain = getRootDomain(request.getHeader("referer"));
+        if (domain != null) {
             return ResponseCookie.from(name, value)
                     .sameSite("None")
                     .secure(true)
-                    .domain(DOMAIN)
+                    .domain(domain)
                     .path(COOKIE_PATH)
                     .httpOnly(READONLY)
                     .maxAge(MAX_AGE)
@@ -42,11 +54,13 @@ public class CookieProvider {
     }
 
     public static ResponseCookie removeSpringCookie(HttpServletRequest request, String name) {
-        if (request.getHeader("referer").contains(DOMAIN)) {
+        String domain = getRootDomain(request.getHeader("referer"));
+
+        if (domain != null) {
             return ResponseCookie.from(name, null)
                     .sameSite("None")
                     .secure(true)
-                    .domain(DOMAIN)
+                    .domain(domain)
                     .path(COOKIE_PATH)
                     .httpOnly(READONLY)
                     .maxAge(0)
