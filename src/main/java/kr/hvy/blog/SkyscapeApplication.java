4e9ebae4f8fe5ef511f7576ce1541c4f72c98d74
Namwon Kim <knw1234@gmail.com>
2024-03-16 21:48:31 +0900

    AOP logging 추가

--- src/main/java/kr/hvy/blog/SkyscapeApplication.java
+++ src/main/java/kr/hvy/blog/SkyscapeApplication.java
@@ -3,9 +3,15 @@ package kr.hvy.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"kr.hvy.blog", "io.github.motolies.aop.log"})
 @MapperScan("kr.hvy.blog.mapper")
+@EnableJpaRepositories(basePackages = {"kr.hvy.blog", "io.github.motolies.aop.log"})
+@EntityScan(basePackages = {"kr.hvy.blog", "io.github.motolies.aop.log"})
+@EnableAsync
 public class SkyscapeApplication {
 
 	public static void main(String[] args) {
