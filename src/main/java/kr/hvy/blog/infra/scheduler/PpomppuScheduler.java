480edcc81941bc90ecbc2616aca03a9a701d58ac
Namwon Kim <knw1234@gmail.com>
2024-06-23 21:40:10 +0900

    스케줄러 운영에서만 동작하도록 수정

--- src/main/java/kr/hvy/blog/infra/scheduler/PpomppuScheduler.java
+++ src/main/java/kr/hvy/blog/infra/scheduler/PpomppuScheduler.java
@@ -20,11 +20,13 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
+@Profile("prod")
 @Component
 @RequiredArgsConstructor
 public class PpomppuScheduler extends AbstractScheduler {
--- src/main/java/kr/hvy/blog/infra/scheduler/PublicIPCheckScheduler.java
+++ src/main/java/kr/hvy/blog/infra/scheduler/PublicIPCheckScheduler.java
@@ -11,11 +11,13 @@ import kr.hvy.blog.module.changeip.dto.PublicIP;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
+@Profile("prod")
 @Component
 @RequiredArgsConstructor
 public class PublicIPCheckScheduler extends AbstractScheduler {
